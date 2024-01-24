89b4f1014546091c3dbc1739c1e03f3ef236eb43
Shwu <shubham.kushwaha@nyunai.com>
2024-01-24 07:53:00 +0000

    update

--- engine/exllamav2/bench.sh
+++ engine/exllamav2/bench.sh
@@ -6,6 +6,8 @@ MODEL_PATH=model/$MODEL_NAME
 EXPORTS=exports/$MODEL_NAME
 METHOD_EXPORTS=$EXPORTS/$METHOD
 
+DATASET_PATH=datasets/wikitext_train.parquet
+
 GPTQ_EXPORTS=$EXPORTS/quant/gptq
 
 mkdir -p $METHOD_EXPORTS
@@ -18,42 +20,42 @@ start_mem_tracker() {
 sleep 3
 start_mem_tracker "baseline"
 sleep 3
-python $METHOD_PATH/test_inference.py -gs 40 -m $MODEL_PATH -s 2>&1 | tee $METHOD_EXPORTS/bench_baseline.log
+python $METHOD_PATH/test_inference.py -ed $DATASET_PATH -gs 40 -m $MODEL_PATH -s 2>&1 | tee $METHOD_EXPORTS/bench_baseline.log
 killall nvidia-smi
 
 # List of model types
 MODEL_4BPW_EXL2="4bpw-exl2"
 # MODEL_6BPW_EXL2="6bpw-exl2"
 MODEL_8BPW_EXL2="8bpw-exl2"
-
+MODEL_2BPW_EXL2="2bpw-exl2"
 
 # Loop through each model type
-for MODEL_TYPE in "$MODEL_4BPW_EXL2" "$MODEL_8BPW_EXL2"; do
+for MODEL_TYPE in "$MODEL_4BPW_EXL2" "$MODEL_8BPW_EXL2" "$MODEL_2BPW_EXL2"; do
     MODEL_PATH=$METHOD_EXPORTS/$MODEL_TYPE
 
     sleep 3
     start_mem_tracker $MODEL_TYPE
     sleep 3
     # Run llama-bench for the current model type
-    python $METHOD_PATH/test_inference.py -gs 40 -m $MODEL_PATH -s 2>&1 | tee $METHOD_EXPORTS/bench_$MODEL_TYPE.log
+    python $METHOD_PATH/test_inference.py -ed $DATASET_PATH -gs 40 -m $MODEL_PATH -s 2>&1 | tee $METHOD_EXPORTS/bench_$MODEL_TYPE.log
 
     # Stop memory tracker for the current model type
     killall nvidia-smi
 done
 
-# Loop through each model type
-for MODEL_TYPE in "$MODEL_4BPW_EXL2" "$MODEL_8BPW_EXL2"; do
-    MODEL_PATH=$METHOD_EXPORTS/$MODEL_TYPE
+# # Loop through each model type
+# for MODEL_TYPE in "$MODEL_4BPW_EXL2" "$MODEL_8BPW_EXL2"; do
+#     MODEL_PATH=$METHOD_EXPORTS/$MODEL_TYPE
 
-    sleep 3
-    start_mem_tracker "$MODEL_TYPE"_prompt
+#     sleep 3
+#     start_mem_tracker "$MODEL_TYPE"_prompt
 
-    # Run llama-bench for the current model type
-    python $METHOD_PATH/test_inference.py -gs 40 -m $MODEL_PATH -p "What is the meaning of life?" 2>&1 | tee $METHOD_EXPORTS/bench_prompt_$MODEL_TYPE.log
+#     # Run llama-bench for the current model type
+#     python $METHOD_PATH/test_inference.py -ed $DATASET_PATH -gs 40 -m $MODEL_PATH -p "What is the meaning of life?" 2>&1 | tee $METHOD_EXPORTS/bench_prompt_$MODEL_TYPE.log
 
-    # Stop memory tracker for the current model type
-    killall nvidia-smi
-done
+#     # Stop memory tracker for the current model type
+#     killall nvidia-smi
+# done
 
 # autogptq
 MODEL_Q4_128G=q4_128g
@@ -73,21 +75,21 @@ for MODEL_TYPE in "$MODEL_Q4_128G"; do
     start_mem_tracker "$MODEL_TYPE"_gptq
 
     # Run llama-bench for the current model type
-    python $METHOD_PATH/test_inference.py -gs 40 -m $MODEL_PATH -s 2>&1 | tee $METHOD_EXPORTS/bench_$MODEL_TYPE.log
+    python $METHOD_PATH/test_inference.py -ed $DATASET_PATH -gs 40 -m $MODEL_PATH -s 2>&1 | tee $METHOD_EXPORTS/bench_$MODEL_TYPE.log
 
     # Stop memory tracker for the current model type
     killall nvidia-smi
 done
 
-for MODEL_TYPE in "$MODEL_Q4_128G"; do
-    MODEL_PATH=$GPTQ_EXPORTS/$MODEL_TYPE
+# for MODEL_TYPE in "$MODEL_Q4_128G"; do
+#     MODEL_PATH=$GPTQ_EXPORTS/$MODEL_TYPE
 
-    sleep 3
-    start_mem_tracker "$MODEL_TYPE"_gptq_prompt
+#     sleep 3
+#     start_mem_tracker "$MODEL_TYPE"_gptq_prompt
 
-    # Run llama-bench for the current model type
-    python $METHOD_PATH/test_inference.py -gs 40 -m $MODEL_PATH -p "What is the meaning of life?" 2>&1 | tee $METHOD_EXPORTS/bench_prompt_$MODEL_TYPE.log
+#     # Run llama-bench for the current model type
+#     python $METHOD_PATH/test_inference.py -ed $DATASET_PATH -gs 40 -m $MODEL_PATH -p "What is the meaning of life?" 2>&1 | tee $METHOD_EXPORTS/bench_prompt_$MODEL_TYPE.log
 
-    # Stop memory tracker for the current model type
-    killall nvidia-smi
-done
+#     # Stop memory tracker for the current model type
+#     killall nvidia-smi
+# done
--- engine/exllamav2/generate.sh
+++ engine/exllamav2/generate.sh
@@ -9,6 +9,8 @@ METHOD_EXPORTS=$EXPORTS/$METHOD
 
 EXL2_TEMP=$METHOD_EXPORTS/temp
 
+HF_EXPORTS=$EXPORTS/quant/hf
+
 mkdir -p $METHOD_EXPORTS
 mkdir -p $EXL2_TEMP
 
@@ -17,38 +19,51 @@ start_mem_tracker() {
     nvidia-smi --format=csv,nounits --query-gpu=utilization.gpu,utilization.memory,memory.used,memory.free,memory.total -lms 100 > $METHOD_EXPORTS/gpu_mem_usage_$1.csv &
 }
 
-# run measurement
-start_mem_tracker "measurement"
-python $METHOD_PATH/convert.py \
--i $MODEL_PATH \
--o $EXL2_TEMP \
--nr \
--om $METHOD_EXPORTS/measurement.json
-killall nvidia-smi
-rm -rf $EXL2_TEMP/*
+# # run measurement
+# start_mem_tracker "measurement"
+# python $METHOD_PATH/convert.py \
+# -i $MODEL_PATH \
+# -o $EXL2_TEMP \
+# -nr \
+# -om $METHOD_EXPORTS/measurement.json
+# killall nvidia-smi
+# rm -rf $EXL2_TEMP/*
 
-# quantized 8.0 bpw
-MODEL_8BPW_EXL2=$METHOD_EXPORTS/8bpw-exl2
-start_mem_tracker "quantize_8bpw"
+# # quantized 8.0 bpw
+# MODEL_8BPW_EXL2=$METHOD_EXPORTS/8bpw-exl2
+# start_mem_tracker "quantize_8bpw"
+# python $METHOD_PATH/convert.py \
+#     -i $MODEL_PATH \
+#     -o $EXL2_TEMP \
+#     -nr \
+#     -m $METHOD_EXPORTS/measurement.json \
+#     -cf $MODEL_8BPW_EXL2 \
+#     -b 8.0
+# killall nvidia-smi
+# rm -rf $EXL2_TEMP/*
+
+# # quantized 4.0 bpw
+# MODEL_4BPW_EXL2=$METHOD_EXPORTS/4bpw-exl2
+# start_mem_tracker "quantize_4bpw"
+# python $METHOD_PATH/convert.py \
+#     -i $MODEL_PATH \
+#     -o $EXL2_TEMP \
+#     -nr \
+#     -m $METHOD_EXPORTS/measurement.json \
+#     -cf $MODEL_4BPW_EXL2 \
+#     -b 4.0
+# killall nvidia-smi
+# rm -rf $EXL2_TEMP/*
+
+# quantized 2.0 bpw
+MODEL_2BPW_EXL2=$METHOD_EXPORTS/2bpw-exl2
+start_mem_tracker "quantize_2bpw"
 python $METHOD_PATH/convert.py \
     -i $MODEL_PATH \
     -o $EXL2_TEMP \
     -nr \
     -m $METHOD_EXPORTS/measurement.json \
-    -cf $MODEL_8BPW_EXL2 \
-    -b 8.0
-killall nvidia-smi
-rm -rf $EXL2_TEMP/*
-
-# quantized 4.0 bpw
-MODEL_4BPW_EXL2=$METHOD_EXPORTS/4bpw-exl2
-start_mem_tracker "quantize_4bpw"
-python $METHOD_PATH/convert.py \
-    -i $MODEL_PATH \
-    -o $EXL2_TEMP \
-    -nr \
-    -m $METHOD_EXPORTS/measurement.json \
-    -cf $MODEL_4BPW_EXL2 \
-    -b 4.0
+    -cf $MODEL_2BPW_EXL2 \
+    -b 2.0
 killall nvidia-smi
 rm -rf $EXL2_TEMP
